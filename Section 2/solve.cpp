c7b35449de1eff24c88dce8118e4bc45608fde7d
DoYoung <guckka94@gmail.com>
2024-09-30 22:49:45 +0900

    문제풀이.md - 2583, 1992

--- Section 2/solve.cpp
+++ Section 2/solve.cpp
@@ -2,71 +2,55 @@
 
 using namespace std;
 
-int T, N, M, K;
-int cnt = 0;
+int N;
+char arr[70][70] = {'0'};
 
-int dirY[] = {1, 0, -1, 0};
-int dirX[] = {0, 1, 0, -1};
-
-void dfs(int arr[][54], bool visited[][54], int y, int x)
+string solve(int y, int x, int width)
 {
-    if (visited[y][x])
-        return;
+    if (width == 1)
+        return string(1, arr[y][x]);
 
-    visited[y][x] = 1;
+    char now = arr[y][x];
+    string ans = "";
 
-    for (int i = 0; i < 4; i++)
+    for (int j = y; j < y + width; j++)
     {
-        int dy = y + dirY[i];
-        int dx = x + dirX[i];
+        for (int i = x; i < x + width; i++)
+        {
+            if (now != arr[j][i])
+            {
+                ans += '(';
+                ans += solve(y, x, width / 2);
+                ans += solve(y, x + width / 2, width / 2);
+                ans += solve(y + width / 2, x, width / 2);
+                ans += solve(y + width / 2, x + width / 2, width / 2);
+                ans += ')';
 
-        if (dy < 0 || dy >= N || dx < 0 || dx >= M)
-            continue;
-        if (visited[dy][dx])
-            continue;
-        if (arr[dy][dx] == 0)
-            continue;
-
-        dfs(arr, visited, dy, dx);
+                return ans;
+            }
+        }
     }
+
+    return string(1, arr[y][x]);
 }
+
 int main()
 {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin >> T;
+    cin >> N;
 
-    while (T--)
+    for (int j = 0; j < N; j++)
     {
-        cin >> M >> N >> K;
+        string input;
+        cin >> input;
 
-        int arr[54][54] = {0};
-        bool visited[54][54] = {0};
-
-        for (int i = 0; i < K; i++)
-        {
-            int y, x;
-            cin >> x >> y;
-
-            arr[y][x] = 1;
-        }
-
-        for (int j = 0; j < N; j++)
-        {
-            for (int i = 0; i < M; i++)
-            {
-                if (visited[j][i])
-                    continue;
-                if (arr[j][i] == 0)
-                    continue;
-
-                dfs(arr, visited, j, i);
-                cnt++;
-            }
-        }
-
-        cout << cnt << '\n';
-        cnt = 0;
+        for (int i = 0; i < N; i++)
+            arr[j][i] = input[i];
     }
+
+    cout << solve(0, 0, N) << '\n';
+
+    return 0;
 }
