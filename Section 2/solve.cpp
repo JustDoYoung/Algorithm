750a27e61e57478fabce29748e604e7e1ae40281
DoYoung <guckka94@gmail.com>
2024-11-20 15:06:20 +0900

    Section 2 - 문풀

--- Section 2/solve.cpp
+++ Section 2/solve.cpp
@@ -3,6 +3,48 @@
 using namespace std;
 
 int N;
+char arr[65][65];
+
+string quad(int y, int x, int w)
+{
+    string ret = "";
+    stack<tuple<int, int, int, string>> s;
+    s.push({y, x, w, ""});
+
+    while (!s.empty())
+    {
+        auto [cy, cx, w, close] = s.top();
+        char now = arr[cy][cx];
+        s.pop();
+
+        bool same = true;
+        for (int j = cy; j < cy + w; j++)
+        {
+            for (int i = cx; i < cx + w; i++)
+            {
+                if (arr[j][i] != now)
+                {
+                    ret += "(";
+                    s.push({cy + w / 2, cx + w / 2, w / 2, close + ")"});
+                    s.push({cy + w / 2, cx, w / 2, ""});
+                    s.push({cy, cx + w / 2, w / 2, ""});
+                    s.push({cy, cx, w / 2, ""});
+                    same = false;
+                    break;
+                }
+            }
+            if (!same)
+                break;
+        }
+
+        if (same)
+        {
+            ret += now + close;
+        }
+    }
+
+    return ret;
+}
 
 int main()
 {
@@ -11,19 +53,18 @@ int main()
 
     cin >> N;
 
-    int num = 666;
-
-    while (true)
+    for (int i = 0; i < N; i++)
     {
-        string num_s = to_string(num);
-        if (num_s.find("666") != string::npos)
-            N--;
-        if (N == 0)
-            break;
-        num++;
+        string input;
+        cin >> input;
+
+        for (int j = 0; j < N; j++)
+        {
+            arr[i][j] = input[j];
+        }
     }
 
-    cout << num << '\n';
+    cout << quad(0, 0, N) << '\n';
 
     return 0;
 }
