4d5c09633a9811f3e556b40ddb7df2f3f84d118c
DoYoung <guckka94@gmail.com>
2024-12-02 16:41:58 +0900

    문풀 - 2636

--- Section 2/solve.cpp
+++ Section 2/solve.cpp
@@ -2,58 +2,54 @@
 
 using namespace std;
 
-int N, M;
-int mapGrid[10][10];
-bool visited[10][10];
+int N, M, cnt, ans1, ans2;
+int grid[103][103];
+bool visited[103][103];
 
-vector<pair<int, int>> virus;
-vector<pair<int, int>> wall;
+int dy[4] = {0, 0, 1, -1};
+int dx[4] = {1, -1, 0, 0};
 
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
-
-void SpreadVirus(int y, int x)
+void melt(int y, int x)
 {
     visited[y][x] = true;
 
+    if (grid[y][x] == 1)
+    {
+        grid[y][x] = 0;
+        cnt++;
+        return;
+    }
+
     for (int i = 0; i < 4; i++)
     {
-        int nx = x + dx[i];
         int ny = y + dy[i];
+        int nx = x + dx[i];
 
-        if (nx < 0 || nx >= M || ny < 0 || ny >= N)
+        if (ny < 0 || ny >= N || nx < 0 || nx >= M)
             continue;
-        if (visited[ny][nx] || mapGrid[ny][nx] == 1)
+        if (visited[ny][nx])
             continue;
 
-        SpreadVirus(ny, nx);
+        melt(ny, nx);
     }
+
+    return;
 }
 
-int solve()
+bool isAllMelted()
 {
-    for (auto v : virus)
-    {
-        int y = v.first;
-        int x = v.second;
-
-        SpreadVirus(y, x);
-    }
-
-    // check walls
-    int cnt = 0;
-
     for (int j = 0; j < N; j++)
     {
         for (int i = 0; i < M; i++)
         {
-            if (mapGrid[j][i] == 0 && visited[j][i] == false)
-                cnt++;
+            if (grid[j][i] == 1)
+                return false;
         }
     }
 
-    return cnt;
+    return true;
 }
+
 int main()
 {
     cin.tie(NULL);
@@ -65,34 +61,25 @@ int main()
     {
         for (int i = 0; i < M; i++)
         {
-            cin >> mapGrid[j][i];
-            if (mapGrid[j][i] == 2)
-                virus.push_back({j, i});
-            else if (mapGrid[j][i] == 0)
-                wall.push_back({j, i});
+            cin >> grid[j][i];
         }
     }
 
-    int ret = 0;
-    for (int a = 0; a < wall.size() - 2; a++)
+    while (true)
     {
-        for (int b = a + 1; b < wall.size() - 1; b++)
-        {
-            for (int c = b + 1; c < wall.size(); c++)
-            {
-                fill(&visited[0][0], &visited[0][0] + 10 * 10, false);
-                mapGrid[wall[a].first][wall[a].second] = 1;
-                mapGrid[wall[b].first][wall[b].second] = 1;
-                mapGrid[wall[c].first][wall[c].second] = 1;
-                ret = max(ret, solve());
-                mapGrid[wall[a].first][wall[a].second] = 0;
-                mapGrid[wall[b].first][wall[b].second] = 0;
-                mapGrid[wall[c].first][wall[c].second] = 0;
-            }
-        }
+        fill(&visited[0][0], &visited[0][0] + 103 * 103, false);
+        cnt = 0;
+
+        melt(0, 0);
+        ans2 = cnt;
+        ans1++;
+
+        if (isAllMelted())
+            break;
     }
 
-    cout << ret << '\n';
+    cout << ans1 << '\n'
+         << ans2 << '\n';
 
     return 0;
 }
